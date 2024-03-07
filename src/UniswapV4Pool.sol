98bfc2bee5223d7c0537d49e83a7e2fe343a34f5
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2024-03-06 22:48:48 -0500

    Updating Pool

--- src/UniswapV4Pool.sol
+++ src/UniswapV4Pool.sol
@@ -1,35 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.19;
 
-import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
-import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
-import {Currency, CurrencyLibrary} from "v4-core/src/types/Currency.sol";
-import {PoolInitializeTest} from "v4-core/test/PoolManagerInitialize.t.sol";
-import {PoolModifyLiquidityTest} from "v4-core/test/PoolModifyLiquidityTest.sol";
-import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
-import {TickMath} from "v4-core/libraries/TickMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IPoolManager} from "lib/v4-core/src/interfaces/IPoolManager.sol";
+import {PoolInitializeTest} from "lib/v4-core/src/test/PoolInitializeTest.sol";
+import {IHooks} from "lib/v4-core/src/interfaces/IHooks.sol";
+import {PoolKey} from "lib/v4-core/src/types/PoolKey.sol";
+import {Currency, CurrencyLibrary} from "lib/v4-core/src/types/Currency.sol";
 
-contract UniswapV4Pool {
-    PoolInitializeTest public initializeRouter;
-    PoolModifyLiquidityTest public lpRouter;
-    PoolSwapTest public swapRouter;
-    address hook;
+contract V4Pool {
+    using CurrencyLibrary for Currency;
 
-    constructor(
-        address _initializeRouter,
-        address _lpRouter,
-        address _swapRouter,
-        address _hook
-    ) {
-        initializeRouter = PoolInitializeTest(_initializeRouter);
-        lpRouter = PoolModifyLiquidityTest(_lpRouter);
-        swapRouter = PoolSwapTest(_swapRouter);
-        hook = _hook;
-    }
+    PoolInitializeTest initializeRouter = PoolInitializeTest(address(0x02));
 
-    function initializePool(
+    function init(
         address token0,
         address token1,
         uint24 swapFee,
@@ -42,49 +25,13 @@ contract UniswapV4Pool {
             (token0, token1) = (token1, token0);
         }
 
-        PoolKey memory poolKey = PoolKey({
+        PoolKey memory pool = PoolKey({
             currency0: Currency.wrap(token0),
             currency1: Currency.wrap(token1),
             fee: swapFee,
             tickSpacing: tickSpacing,
             hooks: IHooks(hook)
         });
-
-        initializeRouter.initialize(poolKey, sqrtPriceX96, hookData);
-    }
-
-    function addLiquidity(
-        PoolKey memory poolKey,
-        int24 tickLower,
-        int24 tickUpper,
-        int256 liquidity,
-        bytes calldata hookData
-    ) external {
-        lpRouter.modifyLiquidity(
-            poolKey,
-            IPoolManager.ModifyLiquidityParams({tickLower: tickLower, tickUpper: tickUpper, liquidityDelta: liquidity}),
-            hookData
-        );
-    }
-
-    function swap(
-        PoolKey memory key,
-        int256 amountSpecified,
-        bool zeroForOne,
-        bytes memory hookData
-    ) internal {
-        IPoolManager.SwapParams memory params = IPoolManager.SwapParams({
-            zeroForOne: zeroForOne,
-            amountSpecified: amountSpecified,
-            sqrtPriceLimitX96: zeroForOne ? TickMath.MIN_SQRT_RATIO + 1 : TickMath.MAX_SQRT_RATIO - 1
-        });
-
-        PoolSwapTest.TestSettings memory testSettings = PoolSwapTest.TestSettings({
-            withdrawTokens: true,
-            settleUsingTransfer: true,
-            currencyAlreadySent: false
-        });
-
-        swapRouter.swap(key, params, testSettings, hookData);
+        initializeRouter.initialize(pool, sqrtPriceX96, hookData);
     }
 }
