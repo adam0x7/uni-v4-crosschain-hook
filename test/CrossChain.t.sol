df6d2e6e1ff9916be53320738ffc87405d6d027f
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2024-03-25 21:30:20 -0500

    Update CrossChain.t.sol

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
-import {Deployers} from "v4-core/test/utils/Deployers.sol";
+import {Deployers} from "lib/v4-core/test/utils/Deployers.sol";
+import {Hooks} from "lib/v4-core/src/libraries/Hooks.sol";
+import {HookMiner} from "./utils/HookMiner.sol";
+import {CrossChainHook} from "src";
 
 /* 
 
@@ -12,6 +15,8 @@ import {Deployers} from "v4-core/test/utils/Deployers.sol";
 
 
 contract CrossChainHookTest is Test, Deployers{ 
+
+    CrossChainHook
 
     string eth_fork = vm.envString("ETH_RPC"); 
     string poly_fork = vm.envString("POLY_RPC");
@@ -26,8 +31,8 @@ contract CrossChainHookTest is Test, Deployers{
 
         Deployers.deployFreshManagerAndRouters();
         Deployers.deployMintAndApprove2Currencies();
-        uint160 flag = 
-
+        uint160 flag = uint160(Hooks.AFTER_SWAP_FLAG); // flag is used for hook bytecode
+        (address hookAddress, uint160 salt) = HookMiner.find(address(this), flag, creationCode, constructorArgs);
 
 
 
